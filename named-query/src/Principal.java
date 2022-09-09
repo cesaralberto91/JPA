8db86ca9ede7aee39a3870a0f3cabcb394688144
César Alberto da Silva <cesar@ifsp.edu.br>
2022-09-09 15:37:14 -0300

    Projeto Named Query

---
+++ named-query/src/Principal.java
@@ -0,0 +1,55 @@
+
+import dao.PessoaDAO;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+import model.Pessoa;
+
+/**
+ *
+ * @author aluno
+ */
+public class Principal {
+    
+    public static void main(String[] args) {
+        
+        PessoaDAO pessoaDAO = new PessoaDAO();
+        
+        for (int i = 0; i < 10;) {
+            
+            Pessoa p = new Pessoa("Pessoa " + i, 
+                    new Date(2000+i, i, i), new BigDecimal(1000 * ++i));
+            
+            pessoaDAO.inserir(p);
+        }
+        
+        
+        //Testar a consulta
+        Pessoa pessoaRetornada = pessoaDAO.buscarPorId(5);
+        
+        //criar o método toString na classe Pessoa
+        System.out.println(pessoaRetornada);
+        
+        System.out.println("\n\nPessoas que o nome contém 5");
+        List<Pessoa> pessoas = pessoaDAO.buscarPorNome("5");
+        for (Pessoa pessoa : pessoas) {
+            System.out.println(pessoa);
+        }
+        
+        
+        System.out.println("\n\nPessoas que ganham mais que R$ 5.000,00");
+        pessoas = pessoaDAO.buscarPorSalario(new BigDecimal(5000));
+        for (Pessoa pessoa : pessoas) {
+            System.out.println(pessoa);
+        }
+        
+        
+        System.out.println("\n\nPessoas que nasceram após 2005");
+        pessoas = pessoaDAO.buscarPorDataNascimento(new Date(2005,0,1));
+        for (Pessoa pessoa : pessoas) {
+            System.out.println(pessoa);
+        }
+        
+    }
+    
+}
---
+++ named-query/src/dao/PessoaDAO.java
@@ -0,0 +1,51 @@
+package dao;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import model.Pessoa;
+
+/**
+ *
+ * @author aluno
+ */
+public class PessoaDAO extends AbstractDAO<Pessoa> {
+
+    public Pessoa buscarPorId(long id) {
+        EntityManager em = getEntityManager();
+        TypedQuery<Pessoa> query = em.createNamedQuery(
+                "Pessoa.buscarPorId", Pessoa.class);
+        query.setParameter("id", id);
+
+        return query.getSingleResult();
+    }
+
+    public List<Pessoa> buscarPorNome(String nome) {
+        EntityManager em = getEntityManager();
+        TypedQuery<Pessoa> query = em.createNamedQuery(
+                "Pessoa.buscarPorNome", Pessoa.class);
+        query.setParameter("nome", "%" + nome + "%");
+
+        return query.getResultList();
+    }
+    
+    public List<Pessoa> buscarPorSalario(BigDecimal salario) {
+        EntityManager em = getEntityManager();
+        TypedQuery<Pessoa> query = em.createNamedQuery(
+                "Pessoa.buscarPorSalario", Pessoa.class);
+        query.setParameter("salario", salario);
+
+        return query.getResultList();
+    }
+    
+    public List<Pessoa> buscarPorDataNascimento(Date dataNascimento) {
+        EntityManager em = getEntityManager();
+        TypedQuery<Pessoa> query = em.createNamedQuery(
+                "Pessoa.buscarPorDataNascimento", Pessoa.class);
+        query.setParameter("dataNascimento", dataNascimento);
+
+        return query.getResultList();
+    }
+}
